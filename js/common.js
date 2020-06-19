a07eaae3d1dff612bab215235118fb70cc9ad505
SHLimon07 <sajjadblanco07@gmail.com>
2020-06-20 02:46:49 +0600

    navbar collase uupdated

--- js/common.js
+++ js/common.js
@@ -1,41 +1,11 @@
 
 // This part is for responsive navBar
 
-var collapseState = 1;
 
 function toogleCollapse () {
 	
 	var navId = document.getElementById("navigation");
+	navId.classList.toggle("navToogle");
 
-	navId.style.zIndex = '-1';
-
-	if(collapseState==1)
-	{
-		collapseState = 0;
-
-		//waiting for the nav to come down
-		setTimeout(function front(){
-
-			//then bring it to the front to make it funtional
-			//otherwise the links cannot be clicked
-
-			navId.style.zIndex = '1';
-
-		},500);		//200+300<-froms css transition
-	}
-	else
-	{
-
-		navId.style.zIndex = '-1';
-		collapseState = 1;
-	}
-
-	//waits for the zindex to work properly
-	setTimeout(function toogle()
-		{
-			navId.classList.toggle("navToogle");
-		}, 200);
-
-	//if the nav is collapsed
 	
 }
